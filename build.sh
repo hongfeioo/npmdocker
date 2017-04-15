a07583d908d5a73c5079e4681001527e1f72df2a
yihf <hongfeio.o@163.com>
2017-04-15 22:41:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker stop NodePingManage ; docker rm NodePingManage
 docker build -t NodePingManageImg ./
-docker run  --volume=/root/NodePingManage/:/NodePingManage:rw  -itd   --env Interval=60     --name=NodePingManage  --net=host     --restart=always   NodePingManageImg:latest 
+docker run  --volume=/home/NodePingManage/:/NodePingManage:rw  -itd   --env Interval=60     --name=NodePingManage  --net=host     --restart=always   NodePingManageImg:latest 
